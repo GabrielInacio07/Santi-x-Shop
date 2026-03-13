dc724bf0078a70961b662c8e4f8469b6893014de
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-03-12 23:47:45 -0300

    projeto de um E-commerce

---
+++ Santix/src/main/java/com/ecommerce/Santix/SantixShopApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.Santix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantixShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantixShopApplication.class, args);
+	}
+
+}
---
+++ Santix/src/test/java/com/ecommerce/Santix/SantixShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.Santix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantixShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
